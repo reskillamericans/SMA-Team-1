cf0fff153ec457b4a794ec9266272e234e0a6025
Jessica Lam <49288633+jesslam@users.noreply.github.com>
2021-06-25 13:07:59 -0700

    Update app urls and views files

--- smapp/urls.py
+++ smapp/urls.py
@@ -1,12 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import json
+from django.urls import path
+from . import views
 
-# Create your views here.
-def index(request):
-    data = {
-        'Project': 'Social Media Application',
-        'Team': '1',
-    }
-    print(data)
-    return HttpResponse(json.dumps(data))
+urlpatterns = [
+    path('', views.index, name ='index')
+]

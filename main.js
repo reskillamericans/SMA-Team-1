b28d41a2922410bab8d86815450b9159ba3ea8b8
Pia Torain <piatorain@yahoo.com>
2021-07-07 06:05:35 -0700

    I am adding frontend all assets, html and css after creating a new clone

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-var gfg = document.getElementById("fname");
-        gfg.onchange = function (e) {
-            if (gfg.value != '') {
-                e.target.style.border
-                        = "4px dotted red";
-            }
-        };
+//  Add email and password to an object when submitted
+
+var objects = {};
+var form = document.getElementById('form');
+form.onsubmit = function(e){
+    var item = document.getElementById('item').value, email =document.getElementById('email').value, password = document.getElementById('password').value;
+    objects[item] = {'email':email, 'password':password}
+    console.log(JSON.stringify(objects));
+    e.preventDefault();
+}

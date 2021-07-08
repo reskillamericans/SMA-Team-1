985d12aa86a08ed4d8b8b39eb5471caf0ef0fb6f
Pia Torain <piatorain@yahoo.com>
2021-07-08 05:59:50 -0700

    Saving changes made to password files

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-
 //  Add email and password to an object when submitted
 
 var objects = {};
@@ -11,4 +10,61 @@ form.onsubmit = function(e){
     e.preventDefault();
 }
 
+//
+document.addEventListener("DOMContentLoaded", () => {
 
+    const main = document.getElementById("main");
+    const BtnOne = document.getElementById('mainBtn');
+    const email = document.getElementById("emailSent");
+    const BtnTwo = document.getElementById("emailBtnTwo");
+    const reset = document.getElementById("reset");
+    const BtnThree = document.getElementById("confirmBtnThree");
+    const success = document.getElementById("successReset");
+    const BtnFour = document.getElementById("successBtnFour");
+    
+    main.style.display = "block";
+    email.style.display = "none"; 
+    reset.style.display = "none";
+    success.style.display = "none";
+     
+    
+});
+
+const main = document.getElementById("main");
+    const BtnOne = document.getElementById('mainBtn');
+    const email = document.getElementById("emailSent");
+    const BtnTwo = document.getElementById("emailBtnTwo");
+    const reset = document.getElementById("reset");
+    const BtnThree = document.getElementById("confirmBtnThree");
+    const success = document.getElementById("successReset");
+    const BtnFour = document.getElementById("successBtnFour");
+
+BtnOne.addEventListener('click', showEmail);
+    BtnTwo.addEventListener('click', showReset);
+    BtnThree.addEventListener('click', showSuccess);
+    BtnFour.addEventListener('click', showHome);
+
+function showEmail() {
+    main.style.display = "none";
+    reset.style.display = "none";
+    success.style.display = "none";
+    email.style.display = "block"; 
+};
+function showReset() {
+    email.style.display = "none";
+    main.style.display = "none";
+    success.style.display = "none";
+    reset.style.display = "block";
+};
+function showSuccess() {
+    email.style.display = "none";
+    main.style.display = "none";
+    reset.style.display = "none";
+    success.style.display = "block";
+};
+function showHome() {
+    reset.style.display = "none";
+    email.style.display = "none";
+    success.style.display = "none";
+    main.style.display = "block";
+};

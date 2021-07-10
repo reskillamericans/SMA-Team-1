17314d6ddd9f8395ec81fbe41bce8d2c467fcf6a
Pia Torain <piatorain@yahoo.com>
2021-07-10 09:16:20 -0700

    Modified checkboxes.css to meet standards of Figma Design.

--- main.js
+++ main.js
@@ -11,48 +11,48 @@ form.onsubmit = function(e){
 }
 
 //Script for Forgot/Reset password - has error...run correctly only after clicking X in corner 
-document.addEventListener("DOMContentLoaded", () => {
-    const main = document.getElementById("main");
-    const BtnOne = document.getElementById('mainBtn');
-    const emailPopUp = document.getElementById("emailSent");
-    const BtnTwo = document.getElementById("emailBtnTwo");
-    const reset = document.getElementById("reset");
-    const BtnThree = document.getElementById("confirmBtnThree");
-    const success = document.getElementById("successReset");
-    const BtnFour = document.getElementById("successBtnFour");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const main = document.getElementById("main");
+//     const BtnOne = document.getElementById('mainBtn');
+//     const emailPopUp = document.getElementById("emailSent");
+//     const BtnTwo = document.getElementById("emailBtnTwo");
+//     const reset = document.getElementById("reset");
+//     const BtnThree = document.getElementById("confirmBtnThree");
+//     const success = document.getElementById("successReset");
+//     const BtnFour = document.getElementById("successBtnFour");
 
-    emailPopUp .style.display = "none"; 
-    reset.style.display = "none";
-    success.style.display = "none";
-    main.style.display = "block";
+//     emailPopUp .style.display = "none"; 
+//     reset.style.display = "none";
+//     success.style.display = "none";
+//     main.style.display = "block";
     
-    BtnOne.addEventListener('click', showEmail);
-    BtnTwo.addEventListener('click', showReset);
-    BtnThree.addEventListener('click', showSuccess);
-    BtnFour.addEventListener('click', showHome);
+//     BtnOne.addEventListener('click', showEmail);
+//     BtnTwo.addEventListener('click', showReset);
+//     BtnThree.addEventListener('click', showSuccess);
+//     BtnFour.addEventListener('click', showHome);
     
-    function showEmail() {
-        main.style.display = "none";
-        reset.style.display = "none";
-        success.style.display = "none";
-        emailPopUp .style.display = "block"; 
-    };
-    function showReset() {
-        emailPopUp .style.display = "none";
-        main.style.display = "none";
-        success.style.display = "none";
-        reset.style.display = "block";
-    };
-    function showSuccess() {
-        emailPopUp .style.display = "none";
-        main.style.display = "none";
-        reset.style.display = "none";
-        success.style.display = "block";
-    };
-    function showHome() {
-        reset.style.display = "none";
-        emailPopUp.style.display = "none";
-        success.style.display = "none";
-        main.style.display = "block";
-    };
-});
+//     function showEmail() {
+//         main.style.display = "none";
+//         reset.style.display = "none";
+//         success.style.display = "none";
+//         emailPopUp .style.display = "block"; 
+//     };
+//     function showReset() {
+//         emailPopUp .style.display = "none";
+//         main.style.display = "none";
+//         success.style.display = "none";
+//         reset.style.display = "block";
+//     };
+//     function showSuccess() {
+//         emailPopUp .style.display = "none";
+//         main.style.display = "none";
+//         reset.style.display = "none";
+//         success.style.display = "block";
+//     };
+//     function showHome() {
+//         reset.style.display = "none";
+//         emailPopUp.style.display = "none";
+//         success.style.display = "none";
+//         main.style.display = "block";
+//     };
+// });

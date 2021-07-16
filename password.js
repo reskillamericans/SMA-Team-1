1b51b5bba347d5b39254809043e37cbccb42de9b
Pia Torain <piatorain@yahoo.com>
2021-07-16 10:48:32 -0700

    Added more assets.Completed Registration Desktop

--- password.js
+++ password.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("form");
-    const email = document.getElementById("email");
     const main = document.getElementById("main");
     const BtnOne = document.getElementById('mainBtn');
     const emailPopUp = document.getElementById("emailSent");
@@ -8,13 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const reset = document.getElementById("reset");
     const BtnThree = document.getElementById("confirmBtnThree");
     const success = document.getElementById("successReset");
-    const BtnFour = document.getElementById("successBtnFour");
+    
 
-    function isValidEmail(email) {
-        const re =
-          /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
-      }
 
 
     if (main.style.display ="block") {
@@ -57,33 +50,4 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
 });
-function isValidEmail(email) {
-    const re =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-// form.addEventListener('click', function (e) {
-//     e.preventDefault();
-
-    
-// form.addEventListener('submit', function (e) {
-//         e.preventDefault();
-
-    
-//     showEmail();
-
-// });
-
-// function showEmail() {
-//     emailPopUp .style.display = "block"; 
-//     main.style.display = "none";
-//     // reset.style.display = "none";
-//     // success.style.display = "none";
-// };
-
-// BtnOne.addEventListener('click', showEmail);
-  
-// form.addEventListener('submit', function (e) {
-//     e.preventDefault();
 

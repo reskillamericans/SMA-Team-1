9ae0abb3b7c23ad2f2e0ccdb17cfe1079f690639
Jessica Lam <49288633+jesslam@users.noreply.github.com>
2021-07-23 02:41:16 -0700

    Add forms to create-edit-delete posts

--- smapp/views.py
+++ smapp/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Posts
+from .forms import PostForm
 
 def index(request):
     return render(request, 'smapp/index.html')
+
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+    if form.is_valid():
+        post = form.save(commit = False)
+        post.author = request.user
+        post.save()
+    else:
+        form = PostForm()
+    return render(request, 'smapp/create_post.html', {'form':form})
+
+def edit_post(request, pk):
+    post = get_object_or_404(Posts, pk = pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance = post)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.save()
+        else:
+            form = PostForm(instance = post)
+    return(render, 'smapp/edit_post.html', {'form': form})
+
+def delete_post(request, key):
+    post = Posts.objects.get(pk=key)
+    post.delete()
+    return render(request, 'delete_post.html')

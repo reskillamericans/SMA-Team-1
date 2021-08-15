a40af837fcf8275fb341923bdb57c56ffe85fe8b
Pia Torain <piatorain@yahoo.com>
2021-08-15 15:08:28 -0700

    create logout feature

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register, login, forgot_password, password_reset, user_detail
+from .views import register, login, forgot_password, password_reset, user_detail, logout_user
 from smapp.views import index
 
 app_name = "accounts"
@@ -9,6 +9,7 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('user_detail/', user_detail, name='user_detail'),
     path('login/', login, name='login'),
+    path('logout/', logout_user, name='logout'),
     path('forgot_password/', forgot_password, name='forgot_password'),
     path('password_reset/<token>/', password_reset, name='password_reset'),
 ]
